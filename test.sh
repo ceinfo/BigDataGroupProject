b39b82ff56dd01f90a8d8ec3e6251685d3dda813
ceinfo <korecat@gmail.com>
2017-04-15 00:00:50 -0400

    addl scripts for individual values

--- test.sh
+++ test.sh
@@ -8,5 +8,5 @@ hdfsout="src/srctmp.out"
 /usr/bin/hadoop fs -rm -r -f "srctmp.out"
 /usr/bin/hadoop jar /opt/cloudera/parcels/CDH-5.9.0-1.cdh5.9.0.p0.23/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapreduce.job.reduces=10 -D mapreduce.map.memory.mb=18384 -D mapreduce.map.java.opts=-Xms10000m  -files "src/" -mapper "src/map.py $2" -reducer "src/reduce.py" -input "$1" -output "srctmp.out"
 /usr/bin/hadoop fs -getmerge "srctmp.out" "src/srctmp.out"
-grep "Data" $hdfsout | sort -n > "src/output"; grep "Sem" $hdfsout | sort -n >> "src/output"; grep "Valid" $hdfsout | sort -n  >> "src/output"
+grep "Data" $hdfsout | sort -n > "src/output"; grep "Sem" $hdfsout | sort -n >> "src/output"; grep "Valid" $hdfsout | sort -n  >> "src/src$2_output"
 
